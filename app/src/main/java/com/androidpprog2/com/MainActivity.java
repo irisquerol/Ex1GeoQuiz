c2834096a5833a2dfa03569ebb7560013679973b
Iris <irisquerol1998@gmail.com>
2022-02-15 19:05:11 +0100

    ex1 v3

--- app/src/main/java/com/androidpprog2/com/MainActivity.java
+++ app/src/main/java/com/androidpprog2/com/MainActivity.java
@@ -3,8 +3,10 @@ package com.androidpprog2.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView question = (TextView) findViewById(R.id.question);
+
+
         trueBtn = (Button) findViewById(R.id.true_button);
         trueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //click true
-                Toast.makeText(MainActivity.this,
-                        R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                question.setText("The sky is green");
+                Toast t = Toast.makeText(MainActivity.this,
+                        R.string.correct_toast, Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                t.show();
+
             }
         });
         falseBtn = (Button) findViewById(R.id.false_button);
@@ -30,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //click false
-                Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast t = Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                t.show();
             }
         });
 
