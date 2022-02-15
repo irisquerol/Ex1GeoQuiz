da0b35c4a0b36af3f49fdbb34134c42f31986fec
Iris <irisquerol1998@gmail.com>
2022-02-15 18:16:52 +0100

    ex1 v2

--- app/src/main/java/com/androidpprog2/com/MainActivity.java
+++ app/src/main/java/com/androidpprog2/com/MainActivity.java
@@ -3,12 +3,37 @@ package com.androidpprog2.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-//de
+    //de
+    private Button trueBtn;
+    private Button falseBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trueBtn = (Button) findViewById(R.id.true_button);
+        trueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //click true
+                Toast.makeText(MainActivity.this,
+                        R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        falseBtn = (Button) findViewById(R.id.false_button);
+        falseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //click false
+                Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
