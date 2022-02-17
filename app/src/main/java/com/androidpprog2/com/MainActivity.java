8d0bbcc041b5f966db60c7e8b096c951c5170591
martifarre <92933891+martifarre@users.noreply.github.com>
2022-02-18 00:39:44 +0100

    Organized the code in functions, added a reset button and made some minor changes ;)

--- app/src/main/java/com/androidpprog2/com/MainActivity.java
+++ app/src/main/java/com/androidpprog2/com/MainActivity.java
@@ -2,7 +2,6 @@ package com.androidpprog2.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -11,75 +10,88 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int cont = 0;
+    private int count = 0;
+
+    private TextView question;
+
+    private Button trueBtn;
+    private Button falseBtn;
+    private Button restartBtn;
+
+    private String[] questions;
+    private int[] answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String[] questions = getResources().getStringArray(R.array.questions);
-        int[] answers = getResources().getIntArray(R.array.answers);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView question = findViewById(R.id.question);
-        question.setText(questions[cont]);
 
-        Button trueBtn = findViewById(R.id.true_button);
-        Button falseBtn = findViewById(R.id.false_button);
-        trueBtn.setOnClickListener(view -> {
-            //click true
-            if (answers[cont] == 1) {
-                //correct
-                cont++;
-                if (cont > 4) {
-                    question.setText(R.string.finished);
-                    falseBtn.setVisibility(View.INVISIBLE);
-                    trueBtn.setVisibility(View.INVISIBLE);
-                } else {
-                    question.setText(questions[cont]);
-                }
-                Toast t = Toast.makeText(MainActivity.this,
-                        R.string.correct_toast, Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-                t.show();
+        questions = getResources().getStringArray(R.array.questions);
+        answers = getResources().getIntArray(R.array.answers);
 
+        question = findViewById(R.id.question);
+        question.setText(questions[count]);
 
+        trueBtn = findViewById(R.id.true_button);
+        falseBtn = findViewById(R.id.false_button);
+        restartBtn = findViewById(R.id.reset_button);
+
+        restartBtn.setOnClickListener(view -> restartQuiz());
+
+        trueBtn.setOnClickListener(view -> verifyCorrectOrIncorrect(answers[count], 1));
+
+        falseBtn.setOnClickListener(view -> verifyCorrectOrIncorrect(answers[count], 0));
+
+    }
+
+    private void verifyCorrectOrIncorrect(int answer, int isTrue) {
+        if (answer == isTrue) {
+            //correct
+            count++;
+            if (count > 4) {
+                showEndAndRestartButton();
             } else {
-                //incorrect
-                Toast t = Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast, Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-                t.show();
+                question.setText(questions[count]);
             }
+            showToast(1);
+        } else {
+            //incorrect
+            showToast(0);
+        }
+    }
 
+    private void showToast(int type) {
+        Toast t;
 
-        });
+        if (type == 1) {
+            t = Toast.makeText(MainActivity.this,
+                    R.string.correct_toast, Toast.LENGTH_SHORT);
+        } else {
+            t = Toast.makeText(MainActivity.this,
+                    R.string.incorrect_toast, Toast.LENGTH_SHORT);
+        }
 
-        falseBtn.setOnClickListener(view -> {
-            //click false
-            if (answers[cont] == 0) {
-                //correct
-                cont++;
+        t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+        t.show();
+    }
 
-                if (cont > 4) {
-                    question.setText(R.string.finished);
-                    falseBtn.setVisibility(View.INVISIBLE);
-                    trueBtn.setVisibility(View.INVISIBLE);
-                } else {
-                    question.setText(questions[cont]);
-                }
-                Toast t = Toast.makeText(MainActivity.this,
-                        R.string.correct_toast, Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-                t.show();
+    private void showEndAndRestartButton() {
+        question.setText(R.string.finished);
+        question.setGravity(Gravity.CENTER);
 
-            } else {
-                //incorrect
-                Toast t = Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast, Toast.LENGTH_SHORT);
-                t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-                t.show();
-            }
+        falseBtn.setVisibility(View.GONE);
+        trueBtn.setVisibility(View.GONE);
 
-        });
+        restartBtn.setVisibility(View.VISIBLE);
+    }
 
+    private void restartQuiz() {
+        count = 0;
+        question.setText(questions[count]);
+
+        restartBtn.setVisibility(View.GONE);
+
+        falseBtn.setVisibility(View.VISIBLE);
+        trueBtn.setVisibility(View.VISIBLE);
     }
 }
